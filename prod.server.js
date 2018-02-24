afb5740f6880cf359d88bd0139595862dd65e030
liu-mengwei <542221757@qq.com>
2018-02-24 20:51:20 +0800

    test contribution

--- prod.server.js
+++ prod.server.js
@@ -87,8 +87,8 @@ app.use(express.static('./dist'))
 
 module.exports = app.listen(port, function (err) {
   if (err) {
-    console.log(err)
+    console.log(err);
     return
   }
   console.log('Listening at http://localhost:' + port + '\n')
-})
+});
